dc2b966cc819de7bc0dc0ad54a5b0c2dfe3c14e7
tjasaa9 <tjasa9trost9@gmail.com>
2021-08-19 21:20:10 +0200

    FAQ and footer

--- app.js
+++ app.js
@@ -26,3 +26,27 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+//BackUp
+//Get the backUp button
+mybutton = document.getElementById("backUp");
+
+//When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
+    }
+    else{
+        mybutton.style.display = "none";
+    }
+}
+
+//When the user clicks on the button, scroll  to the top of the document
+function backUp(){
+    document.body.scrollTop = 0; //For Safari
+    document.documentElement.scrollTop = 0; //For Chrome, Firefox, IE and Opera
+}
